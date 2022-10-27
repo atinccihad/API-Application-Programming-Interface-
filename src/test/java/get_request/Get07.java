cc253d2877ea000ee5e7d941f382498647c80072
Cihad Atınç <atinccihad@gmail.com>
2022-10-27 23:40:38 +0300

    API

--- src/test/java/get_request/Get07.java
+++ src/test/java/get_request/Get07.java
@@ -75,7 +75,6 @@ public class Get07 extends JsonplaceholderBaseUrl {
         assertTrue("Id'si 5 den Kucuk Olan Title'lardan herhangi bir tanesi delectus aut autem icermemektedir.",
                 titles.stream().anyMatch(t -> t.equals("delectus aut autem")));
 
-
     }
 
 }
