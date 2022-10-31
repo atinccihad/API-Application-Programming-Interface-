44da2226fef5e227345e4aebc06cdda88598d510
Cihad Atınç <atinccihad@gmail.com>
2022-10-31 21:07:01 +0300

    API

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -6,11 +6,10 @@ import java.util.Map;
 public class JsonPlaceHolderTestData {
     public Map<String, Object> expectedDataMethod(Integer userId, String title, Boolean completed) {
 
-
         Map<String, Object> expectedDataMap = new HashMap<>();
-        expectedDataMap.put("userId", 1);
-        expectedDataMap.put("title", "quis ut nam facilis et officia qui");
-        expectedDataMap.put("completed", false);
+        expectedDataMap.put("userId", userId);
+        expectedDataMap.put("title", title);
+        expectedDataMap.put("completed", completed);
 
         return expectedDataMap;
     }
