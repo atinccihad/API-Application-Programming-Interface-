6f571e863fff6f6395efc7844b789d21f61111ac
Cihad Atınç <atinccihad@gmail.com>
2022-11-02 23:05:40 +0300

    API

--- src/test/java/practice/GetRequest05.java
+++ src/test/java/practice/GetRequest05.java
@@ -4,19 +4,30 @@ import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static utilities.Authentication.generateToken;
 
 public class GetRequest05 {
 
     @Test
-    public void test05() {
+    public void tets06() {
 
-        String url = "https://www.gmibank.com/api/tp-customers";
+        String url ="https://www.gmibank.com/api/tp-customers/114351";
 
-        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRjaDgxIiwiYXV0aCI6IlJPTEVfQURNSU4iLCJleHAiOjE2NzAwMDQyNzd9.ZGNWLPbWqtl2htNVkOWTgF_KHJOXFJX8Z0sUFSK66KGccFrMD9TR15xJWZE3FdsVZISELbrYhg3j4nfO0YsSWQ";
-
-        Response response = given().headers("Authorization", "Bearer " + token).when().get(url);
-
+        Response response = given().headers("Authorization","Bearer "+ generateToken()).when().get(url);
         response.prettyPrint();
+        //Matcher class ile musteri bilgilerini dogrulayin
+        response.then().assertThat().body("firstName",equalTo("Della"),
+                "lastName",equalTo("Heaney"),
+                "email",equalTo("ricardo.larkin@yahoo.com"),
+                "mobilePhoneNumber",equalTo("123-456-7893"),"accounts[0].balance",equalTo(11190),
+                "accounts[0].accountType",equalTo("CHECKING"),
+                "accounts[1].balance",equalTo(69700),
+                "accounts[1].accountType",equalTo("CREDIT_CARD"));
 
+
+
+
+        //JsonPatl ile musteri bilgilerini dogrulayin
     }
 }
