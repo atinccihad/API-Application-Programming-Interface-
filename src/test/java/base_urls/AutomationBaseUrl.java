f69eaac0c89b49de12748ca1dcaabf699d1b5df5
Cihad Atınç <atinccihad@gmail.com>
2022-11-02 22:33:04 +0300

    API

---
+++ src/test/java/base_urls/AutomationBaseUrl.java
@@ -0,0 +1,14 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class AutomationBaseUrl {
+    protected RequestSpecification spec;
+
+    @Before
+    public void setup(){
+        spec= new RequestSpecBuilder().setBaseUri("https://automationexercise.com").build();
+    }
+}
