31b6a0bf185aa3acc18be30d85fa313939dc1781
Cihad Atınç <atinccihad@gmail.com>
2022-11-05 18:50:24 +0300

    API

---
+++ src/test/java/post_request/Post04Pojo.java
@@ -0,0 +1,38 @@
+package post_request;
+
+public class Post04Pojo {
+    /*
+         Given
+          1)  https://restful-booker.herokuapp.com/booking
+          2)   {
+                "firstname": "Ali",
+                "lastname": "Can",
+                "totalprice": 999,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2021-09-21",
+                    "checkout": "2021-12-21"
+                 }
+                 "additionalneeds": "Breakfast"
+             }
+        When
+ 		    I send POST Request to the URL
+ 	    Then
+ 		    Status code is 200
+ 		And
+ 		    Response body is like {
+ 		                            "bookingid": 16,
+ 		                            "booking" :{
+                                        "firstname": "Ali",
+                                        "lastname": "Can",
+                                        "totalprice": 999,
+                                        "depositpaid": true,
+                                        "bookingdates": {
+                                            "checkin": "2021-09-21",
+                                            "checkout": "2021-12-21"
+                                        }
+                                        "additionalneeds": "Breakfast"
+                                     }
+                                  }
+     */
+}
