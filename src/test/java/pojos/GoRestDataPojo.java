e5afb9ef4b6076c326255bd1810c0c5e026fb578
Cihad Atınç <atinccihad@gmail.com>
2022-11-05 18:12:54 +0300

    API

---
+++ src/test/java/pojos/GoRestDataPojo.java
@@ -0,0 +1,83 @@
+package pojos;
+
+public class GoRestDataPojo {
+    /*
+        {
+            "meta": null,
+            "data": {
+                "id": 2508,
+                "name": "Sharmila Deshpande VM",
+                "email": "deshpande_sharmila_vm@becker.name",
+                "gender": "female",
+                "status": "active"
+                 }
+     */
+    private Integer id;
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+
+    public GoRestDataPojo() {
+
+    }
+
+    public GoRestDataPojo(Integer id, String name, String email, String gender, String status) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "GoRestDataPojo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
