a39567400405c89513202e2d9dce033052a2efe7
Cihad Atınç <atinccihad@gmail.com>
2022-11-07 22:44:37 +0300

    API

---
+++ src/test/java/practice/H04.java
@@ -0,0 +1,26 @@
+package practice;
+
+import org.junit.Test;
+
+public class H04 {
+
+    @Test
+    public void test04() {
+        //4: Map ile ve Pojo Class ile ayrı ayrı Object Mapper kullanarak yapınız.
+  /*
+       Given
+           1) https://reqres.in/api/users/2
+           2) {
+                "name": "neo"
+               }
+       When
+            I send PATCH Request to the Url
+       Then
+             Status code is 200
+             And response body is like   {
+                                                "name": "neo",
+                                                "updatedAt": "2022-10-02T12:53:21.675Z"
+                                         }
+    */
+    }
+}
