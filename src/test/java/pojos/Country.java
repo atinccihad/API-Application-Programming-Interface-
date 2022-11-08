9ba127736d3c142cd1badebfa8a4ef7fa3cbbcfc
Cihad Atınç <atinccihad@gmail.com>
2022-11-08 23:16:27 +0300

    API

---
+++ src/test/java/pojos/Country.java
@@ -0,0 +1,56 @@
+package pojos;
+
+public class Country {
+    /*
+        “country”: {
+        “id”: 3,
+        “name”: “USA”,
+        “states”: null
+    }
+     */
+    private int id;
+    private String name;
+    private String states;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStates() {
+        return states;
+    }
+
+    public void setStates(String states) {
+        this.states = states;
+    }
+
+    public Country(int id, String name, String states) {
+        this.id = id;
+        this.name = name;
+        this.states = states;
+    }
+
+    public Country() {
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", states='" + states + '\'' +
+                '}';
+    }
+}
