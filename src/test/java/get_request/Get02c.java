fbb3c6c7867e855dcf6a15683428e42206e3cd2e
Cihad Atınç <atinccihad@gmail.com>
2022-11-08 17:35:07 +0300

    API

--- src/test/java/get_request/Get02c.java
+++ src/test/java/get_request/Get02c.java
@@ -5,9 +5,14 @@ import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class Get02c {
-    /*
+
+    @Test
+    public void test02c() {
+        /*
     https://restful-booker.herokuapp.com/booking/ url'ine
     accept type'i "application/json" olan GET request gonderildiginde
     donecek cevap response icin
@@ -16,10 +21,7 @@ public class Get02c {
     Content Type'in "application/json"
     Oldugunu test edin
      */
-
-    @Test
-    public void test02c() {
-        // Get the Url
+        // 1- Get the Url
         String url = "https://restful-booker.herokuapp.com/booking/";
 
         // 2- Beklenen sonuc(expected result) olusturulur
@@ -34,4 +36,37 @@ public class Get02c {
                 .statusCode(200)
                 .contentType("application/json");
     }
+
+    @Test
+    public void test02d() {
+        /*
+    https://restful-booker.herokuapp.com/booking/10011 url'ine
+    accept type'i "application/json" olan GET request gonderildiginde
+    gelen response'un
+
+    HTTP status kodunun 404
+    ve Response body'sinin "Not Found" icerdigini
+    ve Response body'sinin "API" icermedigini
+    test edin
+     */
+        // 1- Get the Url
+        String url = "https://restful-booker.herokuapp.com/booking/10011";
+
+        // 2- Beklenen sonuc(expected result) olusturulur
+        // 3- request gonder
+        Response response = given().accept("application/json").when().get(url);
+        response.prettyPrint();
+
+        // 4- actual result olustur
+        // 5- Dogrulama yap(Do Assertion)
+        String body = response.asString();
+        response.then()
+                .assertThat()
+                .statusCode(404);
+        assertTrue("Response body'si 'Not Found' icermiyor!", body.contains("Not Found"));
+        assertFalse("Response body'si 'API' iceriyor!", body.contains("API"));
+
+        // asString(); ile json formatinda gelen response'u Stringe cevirdik
+    }
+
 }
