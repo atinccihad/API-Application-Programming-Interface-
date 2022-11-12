e3059a60b03497643cae93ca630878e56b2f0068
Cihad Atınç <atinccihad@gmail.com>
2022-11-12 13:27:22 +0300

    API

--- src/test/java/get_request/Get11bTestData.java
+++ src/test/java/get_request/Get11bTestData.java
@@ -4,11 +4,9 @@ import base_urls.JsonplaceholderBaseUrl;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
-import test_data.JsonPlaceHolderTestData;
 import test_data.JsonPlaceHolderTestData2;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -21,7 +19,7 @@ public class Get11bTestData extends JsonplaceholderBaseUrl {
         // Set the expected data
         spec.pathParams("parametre1", "todos", "parametre2", 2);
         JsonPlaceHolderTestData2 expectedObje = new JsonPlaceHolderTestData2();
-        HashMap<String,Object>expectedData = (HashMap<String, Object>) expectedObje.setupTestData();
+        HashMap<String, Object> expectedData = (HashMap<String, Object>) expectedObje.setupTestData();
         System.out.println("expectedData = " + expectedData);
 
         Response response = given()
@@ -33,30 +31,32 @@ public class Get11bTestData extends JsonplaceholderBaseUrl {
         System.out.println("=====================================================================================================================================");
 
         // 1. yontem Matchers class ile assertion islemi
-        response.then().assertThat().statusCode((int)expectedData.get("statusCode"))
+        response.then().assertThat().statusCode((int) expectedData.get("statusCode"))
                 .headers("Via", equalTo(expectedData.get("Via")),
-                        "Server",equalTo(expectedData.get("Server")))
-                .body("userId",equalTo(expectedData.get("userId")),
-                        "title",equalTo(expectedData.get("title")),
-                        "completed",equalTo(expectedData.get("completed")));
+                        "Server", equalTo(expectedData.get("Server")))
+                .body("userId", equalTo(expectedData.get("userId")),
+                        "title", equalTo(expectedData.get("title")),
+                        "completed", equalTo(expectedData.get("completed")));
 
 
         // 2. yontem JsonPath
         JsonPath jsonPath = response.jsonPath();
-        assertEquals("statusCode !!",expectedData.get("statusCode"),response.getStatusCode());
-        assertEquals("Via !!",expectedData.get("Via"),response.getHeader("Via"));
-        assertEquals("Server !!",expectedData.get("Server"),response.getHeader("Server"));
 
-        assertEquals("userId !!",expectedData.get("userId"),jsonPath.getInt("userId"));
-        assertEquals("title !!",expectedData.get("title"),jsonPath.getString("title"));
-        assertEquals("completed !!",expectedData.get("completed"),jsonPath.getBoolean("completed"));
+        assertEquals("statusCode !!", expectedData.get("statusCode"), response.getStatusCode());
+        assertEquals("Via !!", expectedData.get("Via"), response.getHeader("Via"));
+        assertEquals("Server !!", expectedData.get("Server"), response.getHeader("Server"));
+
+        assertEquals("userId !!", expectedData.get("userId"), jsonPath.getInt("userId"));
+        assertEquals("title !!", expectedData.get("title"), jsonPath.getString("title"));
+        assertEquals("completed !!", expectedData.get("completed"), jsonPath.getBoolean("completed"));
 
         // 3. yontem deSerialization
-        // object mapper
-        // pojo class ile birlikte map
+        HashMap<String, Object> actualData = response.as(HashMap.class);
 
-
-
+        System.out.println("actualData = " + actualData);
+        assertEquals("userId !!", expectedData.get("userId"), actualData.get("userId"));
+        assertEquals("title !!", expectedData.get("title"), actualData.get("title"));
+        assertEquals("completed !!", expectedData.get("completed"), actualData.get("completed"));
 
     }
 
