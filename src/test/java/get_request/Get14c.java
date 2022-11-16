87a7409824d1b0b8c7e645ad13103e683f25974e
Cihad Atınç <atinccihad@gmail.com>
2022-11-16 15:18:22 +0300

    API

---
+++ src/test/java/get_request/Get14c.java
@@ -0,0 +1,86 @@
+package get_request;
+
+import base_urls.DummyRestapiBaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.DummyRestapiTestData;
+
+import java.util.*;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get14c extends DummyRestapiBaseUrl {
+    /*
+       https://dummy.restapiexample.com/api/v1/employees  url'ine istek gonderildiginde,
+       Status kodun 200 oldugunu
+       En yuksek maasin 725000 oldugunu,
+       En kucuk yasin 19 oldugunu,
+       Ikinci en yuksek maasin 675000
+           seklinde oldugunu test edin.
+    */
+    @Test
+    public void test14c() {
+        // Set the Url
+        spec.pathParam("parametre1", "employees");
+
+        DummyRestapiTestData expectedObje = new DummyRestapiTestData();
+        HashMap<String, Integer> expectedDataMap = expectedObje.setupTestData02();
+        System.out.println("expectedDataMap = " + expectedDataMap);
+
+        Response response = given()
+                .accept("application/json")
+                .spec(spec)
+                .when()
+                .get("/{parametre1}");
+        // response.prettyPrint();
+
+        //******* 1- De-Serialization yontemi ile ********
+        HashMap<String, Object> actualDataMap = response.as(HashMap.class);
+        System.out.println("actualDataMap = " + actualDataMap);
+
+        // Status kodun 200 oldugunu
+        assertEquals("Status kod 200'den farkli!", expectedDataMap.get("statusCode"), (Integer) response.getStatusCode());
+
+        // En yuksek maasin 725000 oldugunu
+        List<Integer> maasListesi = new ArrayList<>();
+        int dataSize = ((List) actualDataMap.get("data")).size();
+
+        for (int i = 0; i < dataSize; i++) {
+            maasListesi.add((Integer) ((Map) ((List) actualDataMap.get("data")).get(i)).get("employee_salary"));
+        }
+        Collections.sort(maasListesi);
+        assertEquals("En yuksek maas 725000 degil!", expectedDataMap.get("enYuksekMaas"), maasListesi.get(dataSize - 1));
+
+        // En kucuk yasin 19 oldugunu
+        List<Integer> yasListesi = new ArrayList<>();
+
+        for (int i = 0; i < dataSize; i++) {
+            yasListesi.add((Integer) ((Map) ((List) actualDataMap.get("data")).get(i)).get("employee_age"));
+        }
+        Collections.sort(yasListesi);
+        assertEquals("En kucuk yas 19 degil!", expectedDataMap.get("enKucukYas"), yasListesi.get(0));
+
+        // Ikinci en yuksek maasin 675000 seklinde oldugunu test edin.
+        assertEquals("Ikinci yuksek maas 675000 degil!", expectedDataMap.get("ikinciYuksekMaas"), maasListesi.get(dataSize - 2));
+
+        //******** 2- JsonPath yontemi ile ********
+        JsonPath json = response.jsonPath();
+
+        // En yuksek maasin 725000 oldugunu
+        List<Integer> maasListesiJson = json.getList("data.employee_salary");
+        Collections.sort(maasListesiJson);
+        assertEquals("En yuksek maas 725000 degil!", expectedDataMap.get("enYuksekMaas"), maasListesiJson.get(maasListesiJson.size() - 1));
+
+        // En kucuk yasin 19 oldugunu
+        List<Integer> yasListesiJson = json.getList("data.employee_age");
+        Collections.sort(yasListesiJson);
+
+        assertEquals("enKucukYas 19 degil!", expectedDataMap.get("enKucukYas"), yasListesiJson.get(0));
+
+        // Ikinci en yuksek maasin 675000 seklinde oldugunu test edin.
+        assertEquals("Ikinci en yuksek maas 675000 degil!", expectedDataMap.get("ikinciYuksekMaas"), maasListesiJson.get(maasListesiJson.size() - 2));
+
+    }
+}
--- src/test/java/test_data/DummyRestapiTestData.java
+++ src/test/java/test_data/DummyRestapiTestData.java
@@ -5,35 +5,45 @@ import java.util.HashMap;
 import java.util.List;
 
 public class DummyRestapiTestData {
-  /*
-  {
-            "id": 11,
-            "employee_name": "Jena Gaines",
-            "employee_salary": 90560,
-            "employee_age": 30,
-            "profile_image": ""
-        }
-   */
-    public HashMap<String,Object> setuptestData(){
-        List<Integer>yaslar = new ArrayList<>();
+    /*
+    {
+              "id": 11,
+              "employee_name": "Jena Gaines",
+              "employee_salary": 90560,
+              "employee_age": 30,
+              "profile_image": ""
+          }
+     */
+    public HashMap<String, Object> setuptestData() {
+        List<Integer> yaslar = new ArrayList<>();
         yaslar.add(40);
         yaslar.add(21);
         yaslar.add(19);
 
         HashMap<String, Object> onbirinci = new HashMap<String, Object>();
-        onbirinci.put("id",11);
+        onbirinci.put("id", 11);
         onbirinci.put("employee_name", "Jena Gaines");
         onbirinci.put("employee_salary", 90560);
         onbirinci.put("employee_age", 30);
         onbirinci.put("profile_image", "");
 
-        HashMap<String,Object>expectedData = new HashMap<String, Object>();
-        expectedData.put("statusCode",200);
-        expectedData.put("besincicalisan","Airi Satou");
-        expectedData.put("calisansayisi",24);
-        expectedData.put("sondanikincicalisanmaasi",106450);
-        expectedData.put("arananyaslar",yaslar);
-        expectedData.put("onbirincicalisan",onbirinci);
+        HashMap<String, Object> expectedData = new HashMap<String, Object>();
+        expectedData.put("statusCode", 200);
+        expectedData.put("besincicalisan", "Airi Satou");
+        expectedData.put("calisansayisi", 24);
+        expectedData.put("sondanikincicalisanmaasi", 106450);
+        expectedData.put("arananyaslar", yaslar);
+        expectedData.put("onbirincicalisan", onbirinci);
+
+        return expectedData;
+    }
+
+    public HashMap<String, Integer> setupTestData02() {
+        HashMap<String, Integer> expectedData = new HashMap<>();
+        expectedData.put("statusCode", 200);
+        expectedData.put("enYuksekMaas", 725000);
+        expectedData.put("enKucukYas", 19);
+        expectedData.put("ikinciYuksekMaas", 675000);
 
         return expectedData;
     }
