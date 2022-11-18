8f3b7854efb79715b0aaf4f8780960a02420505f
Cihad Atınç <atinccihad@gmail.com>
2022-11-18 22:02:52 +0300

    API

--- src/test/java/test_data/HerokuappTestData.java
+++ src/test/java/test_data/HerokuappTestData.java
@@ -42,6 +42,7 @@ public class HerokuappTestData {
         bookingdates.put("checkin", "2020-09-09");
         bookingdates.put("checkout", "2020-09-21");
 
+
         JSONObject expectedRequest = new JSONObject();
         expectedRequest.put("firstname", "Cihad");
         expectedRequest.put("lastname", "Atınç");
