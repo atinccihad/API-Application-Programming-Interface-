e9672366f541e5d41b1e309d07cfa672ee22f906
Cihad Atınç <atinccihad@gmail.com>
2022-11-21 20:05:59 +0300

    API

--- src/test/java/utilities/Authentication.java
+++ src/test/java/utilities/Authentication.java
@@ -10,34 +10,28 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class Authentication {
-
+/*
     public static void main(String[] args) {
         String guncelToken = generateToken();
         System.out.println(guncelToken);
     }
-
+*/
     public static String generateToken() {
         String username = "Batch81";
         String password = "Batch81+";
-          /*
-        {
-            "password": "string",
-            "rememberMe": true,
-            "username": "string"
-        }
-         */
-        Map<String, Object> postBody = new HashMap<>();
-        postBody.put("username", username);
-        postBody.put("password", password);
-        postBody.put("rememberMe", true);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        map.put("rememberme", "true");
         // "password": "Batch81+",
         //    "rememberMe": true,
         //    "username": "Batch81"
-        String endPoint = "https://www.gmibank.com/api/authenticate";
+        String endPoint = "https://www.medunna.com/api/authenticate";
 
-        Response response = given().contentType(ContentType.JSON).body(postBody).when().post(endPoint);
+        Response response1 = given().contentType(ContentType.JSON).body(map).when().post(endPoint);
 
-        JsonPath token = response.jsonPath();
+        JsonPath token = response1.jsonPath();
 
         return token.getString("id_token");
     }
