9a26de2cf5cd6c4dbc79685169224bd4a9908ea5
Cihad Atınç <atinccihad@gmail.com>
2023-01-26 14:55:43 +0300

    API

--- src/test/java/com30/i08/GetRequest_13.java
+++ src/test/java/com30/i08/GetRequest_13.java
@@ -2,8 +2,10 @@ package com30.i08;
 
 import com30.testBase.RestfulBookerHerokuappTestBase;
 import com30.testData.RestfulBookerHerokuappTestData;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,11 +35,11 @@ public class GetRequest_13 extends RestfulBookerHerokuappTestBase {
     @Test
     public void test() {
         // url olustur
-        specRestful.pathParams("first","booking",
-                "second",1);
+        specRestful.pathParams("first", "booking",
+                "second", 1);
         // expectedData olustur
         RestfulBookerHerokuappTestData expectedObje = new RestfulBookerHerokuappTestData();
-        HashMap<String,Object> expectedDataMap = expectedObje.setUpTestData();
+        HashMap<String, Object> expectedDataMap = expectedObje.setUpTestData();
         System.out.println("expectedDataMap = " + expectedDataMap);
 
         // request gonder
@@ -47,18 +49,33 @@ public class GetRequest_13 extends RestfulBookerHerokuappTestBase {
                 .when()
                 .get("/{first}/{second}");
         response.prettyPrint();
-        
-        // deserialization
-        HashMap<String,Object> actualDataMap = response.as(HashMap.class);
+
+        // 1. yol deserialization
+        HashMap<String, Object> actualDataMap = response.as(HashMap.class);
         System.out.println("actualDataMap = " + actualDataMap);
 
-        assertEquals(expectedDataMap.get("firstname"),actualDataMap.get("firstname"));
-        assertEquals(expectedDataMap.get("lastname"),actualDataMap.get("lastname"));
-        assertEquals(expectedDataMap.get("totalprice"),actualDataMap.get("totalprice"));
-        assertEquals(expectedDataMap.get("depositpaid"),actualDataMap.get("depositpaid"));
-        assertEquals(((Map)expectedDataMap.get("bookingdates")).get("checkin"),
-                     ((Map)actualDataMap.get("bookingdates")).get("checkin"));
-        assertEquals(((Map) expectedDataMap.get("bookingdates")).get("checkout"),
-                     ((Map)actualDataMap.get("bookingdates")).get("checkout"));
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(actualDataMap.get("firstname"), expectedDataMap.get("firstname"));
+        softAssert.assertEquals(actualDataMap.get("lastname"), expectedDataMap.get("lastname"));
+        softAssert.assertEquals(actualDataMap.get("totalprice"), expectedDataMap.get("totalprice"));
+        softAssert.assertEquals(actualDataMap.get("depositpaid"), expectedDataMap.get("depositpaid"));
+        softAssert.assertEquals(((Map) actualDataMap.get("bookingdates")).get("checkin"),
+                ((Map) expectedDataMap.get("bookingdates")).get("checkin"));
+        softAssert.assertEquals(((Map) actualDataMap.get("bookingdates")).get("checkout"),
+                ((Map) expectedDataMap.get("bookingdates")).get("checkout"));
+        softAssert.assertAll();
+
+        // 2. yol JsonPath
+        JsonPath jsonPath = response.jsonPath();
+
+        assertEquals(expectedDataMap.get("firstname"), jsonPath.getString("firstname"));
+        assertEquals(expectedDataMap.get("lastname"), jsonPath.getString("lastname"));
+        assertEquals(expectedDataMap.get("totalprice"), jsonPath.getInt("totalprice"));
+        assertEquals(expectedDataMap.get("depositpaid"), jsonPath.getBoolean("depositpaid"));
+        assertEquals(((Map<?, ?>) expectedDataMap.get("bookingdates")).get("checkin"),
+                jsonPath.getString("bookingdates.checkin"));
+        assertEquals(((Map<?, ?>) expectedDataMap.get("bookingdates")).get("checkout"),
+                jsonPath.getString("bookingdates.checkout"));
+
     }
 }
--- src/test/java/com30/testData/RestfulBookerHerokuappTestData.java
+++ src/test/java/com30/testData/RestfulBookerHerokuappTestData.java
@@ -7,13 +7,13 @@ public class RestfulBookerHerokuappTestData {
     public HashMap<String, Object> setUpTestData() {
 
         HashMap<String, Object> bookingdates = new HashMap<>();
-        bookingdates.put("checkin", "2021-10-10");
-        bookingdates.put("checkout", "2022-08-02");
+        bookingdates.put("checkin", "2019-06-21");
+        bookingdates.put("checkout", "2022-10-29");
 
         HashMap<String, Object> expectedData = new HashMap<>();
-        expectedData.put("firstname", "Eric");
-        expectedData.put("lastname", "Wilson");
-        expectedData.put("totalprice", 728);
+        expectedData.put("firstname", "Sally");
+        expectedData.put("lastname", "Smith");
+        expectedData.put("totalprice", 865);
         expectedData.put("depositpaid", true);
         expectedData.put("bookingdates", bookingdates);
 
