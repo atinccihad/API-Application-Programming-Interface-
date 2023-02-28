9a4871171e072697ad87ed56f6e8c97cbcf14ee4
Cihad Atınç <atinccihad@gmail.com>
2023-02-28 15:33:43 +0300

    API

---
+++ src/test/java/com30/utilities/JsonUtil.java
@@ -0,0 +1,23 @@
+package com30.utilities;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+
+public class JsonUtil {
+    private static ObjectMapper mapper;
+
+    static {
+        mapper = new ObjectMapper();
+    }
+
+    public static <T> T convertJsonToJava(String json, Class<T> cls) {
+        T javaResult = null;
+        try {
+            javaResult = mapper.readValue(json, cls);
+        } catch (IOException e) {
+            System.err.println("json datasi javaya donusturulemedi!");
+        }
+        return javaResult;
+    }
+}
