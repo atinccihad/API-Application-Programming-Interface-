48aec76fd9d0e18faa7cb4599ee598b0d091cb82
Cihad Atınç <atinccihad@gmail.com>
2023-02-28 10:21:06 +0300

    API

--- src/test/java/com30/i12/PostRequestWithPojo_02.java
+++ src/test/java/com30/i12/PostRequestWithPojo_02.java
@@ -1,6 +1,9 @@
 package com30.i12;
 
 import com30.baseUrls.RestfulBookerHerokuappBaseUrl;
+import com30.pojos.JsonPlaceholder_BookingDatesPojo;
+import com30.pojos.JsonPlaceholder_BookingPojo;
+import com30.pojos.JsonPlaceholder_BookingResponsePojo;
 import com30.testData.RestfulBookerHerokuappTestData;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -8,6 +11,7 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class PostRequestWithPojo_02 extends RestfulBookerHerokuappBaseUrl {
     /*
@@ -43,18 +47,38 @@ public class PostRequestWithPojo_02 extends RestfulBookerHerokuappBaseUrl {
     public void test() {
         // url
         specRestful.pathParam("first", "booking");
-        // requestBody ve expected Data ayni oldugu icin tek bir JSONObject kullanilmasi yeterlidir.
-        RestfulBookerHerokuappTestData testData = new RestfulBookerHerokuappTestData();
-        JSONObject expectedRequestData = testData.setUpTestAndRequestData();
-        System.out.println("expectedRequestData = " + expectedRequestData);
+
+        JsonPlaceholder_BookingDatesPojo jsonPlaceholderBookingDatesPojo = new JsonPlaceholder_BookingDatesPojo("2020-09-09", "2020-09-21");
+        System.out.println("jsonPlaceholderBookingDatesPojo = " + jsonPlaceholderBookingDatesPojo);
+
+        JsonPlaceholder_BookingPojo jsonPlaceholder_bookingPojo =
+                new JsonPlaceholder_BookingPojo("Selim", "Ak", 15000, true, jsonPlaceholderBookingDatesPojo);
+        System.out.println("jsonPlaceholder_bookingPojo = " + jsonPlaceholder_bookingPojo);
+
         // request gonder
         Response response = given()
                 .contentType(ContentType.JSON)
                 .spec(specRestful)
                 .auth().basic("admin", "password123")
-                .body(expectedRequestData.toString())
+                .body(jsonPlaceholder_bookingPojo)
                 .when()
                 .post("/{first}");
         response.prettyPrint();
+
+        JsonPlaceholder_BookingResponsePojo actualData = response.as(JsonPlaceholder_BookingResponsePojo.class);
+        // status code is 200
+        assertEquals(200, response.getStatusCode());
+        // "firstname": "cihad"
+        assertEquals(jsonPlaceholder_bookingPojo.getFirstname(), actualData.getBooking().getFirstname());
+        // "lastname": "atinc"
+        assertEquals(jsonPlaceholder_bookingPojo.getLastname(), actualData.getBooking().getLastname());
+        // "totalprice": 123
+        assertEquals(jsonPlaceholder_bookingPojo.getTotalprice(), actualData.getBooking().getTotalprice());
+        // "depositpaid": false
+        assertEquals(jsonPlaceholder_bookingPojo.isDepositpaid(), actualData.getBooking().isDepositpaid());
+        // "checkin": "2021-01-05"
+        assertEquals(jsonPlaceholder_bookingPojo.getBookingDatesPojo().getCheckin(), actualData.getBooking().getBookingDatesPojo().getCheckin());
+        // "checkout": "2021-01-10"
+        assertEquals(jsonPlaceholder_bookingPojo.getBookingDatesPojo().getCheckout(), actualData.getBooking().getBookingDatesPojo().getCheckout());
     }
 }
